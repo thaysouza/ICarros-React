300bddb181e6401b6a5e8dfb1bd32fd2b4bcb0c0
thaysouza <tayanesilvasouzas@gmail.com>
2022-06-26 18:41:42 -0300

    corrigindo bugs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## Projeto Icarros React
+# I Carros | ReactJS |  Gama Academy
+
+### Para gerenciar a versão do NODEJS use o NVM:
+[Clique aqui para acessar documentação](https://github.com/nvm-sh/nvm)
+
--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,20 @@
 const path = require('path');
-const { ClearWebpackPlugin } = require('clear-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'main-bundle-[hash].js',
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.scss'],
-        alias: {
-            '@': path.join(__dirname, 'src')
-        }
-    },
-    plugins: [
-        new ClearWebpackPlugin()
-    ]
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main-bundle-[hash].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+    alias: {
+      '@':  path.join(__dirname, 'src')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
